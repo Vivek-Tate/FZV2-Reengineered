e876eef3987b193f3411c46106f1759580f5e3fe
[Prajwal Patil] <[prajwalpatil.2014@gmail.com]>
2024-05-07 22:26:04 +0100

    Fixed the CallBashTest.py

--- FinderZ/tests/CallBashTest.py
+++ FinderZ/tests/CallBashTest.py
@@ -4,8 +4,8 @@ from FinderZV2 import callBash
 
 class TestCallBash(unittest.TestCase):
     def test_callBash(self):
-        directory_path = r'Testing-FinderZ/helloworld.bat'
+        directory_path = r"Testing-FinderZ\helloworld.bat"
         expected_output = None
-        actual_output = callBash.runFile("D:\Sem2\SR-Test\Testing-FinderZ/helloworld.bat")
+        actual_output = callBash.runFile(directory_path)
 
         self.assertEqual(expected_output, actual_output, f"Test case passed")
