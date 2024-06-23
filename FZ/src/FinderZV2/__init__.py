6ed78bab5cc45abd9979b5bf8bc76c606f853e2d
Vivek Shravan Tate <142102493+Vivek-Tate@users.noreply.github.com>
2024-06-23 15:59:24 +0100

    Updated __init__.py

--- FZ/src/FinderZV2/__init__.py
+++ FZ/src/FinderZV2/__init__.py
@@ -1,9 +1,3 @@
-# from FinderZV2 import GatherInfo
-# from FinderZV2 import fileOperands
-# from FinderZV2 import Synchronize
-# from FinderZV2 import Backup
-# from FinderZV2 import CallBash
-
 from .GatherInfo import GatherInfo
 from .Logging import Logging
 from .Backup import Backup
